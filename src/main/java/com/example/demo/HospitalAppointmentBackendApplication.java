6899264ac582be55871da80b460a14018d50c61e
Navneet Ranjan <ridestatus07@gmail.com>
2025-11-20 19:28:33 +0530

    added gitignore

--- src/main/java/com/example/demo/HospitalAppointmentBackendApplication.java
+++ src/main/java/com/example/demo/HospitalAppointmentBackendApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HospitalAppointmentBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HospitalAppointmentBackendApplication.class, args);
+
+        SpringApplication.run(HospitalAppointmentBackendApplication.class, args);
 	}
 
 }
